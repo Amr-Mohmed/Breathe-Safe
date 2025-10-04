83b6f20bd0c01a75e3c58c04ecbb25bb8c99ea8c
youssefibrahim258 <youssefibrahim1995.2003@gmail.com>
2025-10-04 07:03:53 +0300

    depenedency injection is added

--- web/backend/Web.Api/Program.cs
+++ web/backend/Web.Api/Program.cs
@@ -1,22 +1,30 @@
 using DataConfiguration;
+using IRepository.Generic;
+using IService;
 using Microsoft.EntityFrameworkCore;
+using Repositories.Generic;
+using Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
- builder.Services.AddOpenApi();
+builder.Services.AddOpenApi();
 builder.Services.AddControllers();
+
+builder.Services.AddScoped<IAirQualityPredictionService, AirQualityPredictionSerivce>();
+builder.Services.AddScoped<ISubscriberService, SubscriberService>();
+builder.Services.AddScoped(typeof(IRepository<>), typeof(Repository<>));
 
 builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("default")));
 
 var app = builder.Build();
 
- 
+
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
 }
 
-app.UseHttpsRedirection(); 
+app.UseHttpsRedirection();
 app.MapControllers();
 
-app.Run(); 
+app.Run();
