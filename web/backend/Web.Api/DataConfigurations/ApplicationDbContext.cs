7992c7de77a4d02959a6d646d2e8235f383c571d
youssefibrahim258 <youssefibrahim1995.2003@gmail.com>
2025-10-04 06:43:24 +0300

    Add efcore configurations.

--- web/backend/Web.Api/DataConfigurations/ApplicationDbContext.cs
+++ web/backend/Web.Api/DataConfigurations/ApplicationDbContext.cs
@@ -5,12 +5,18 @@ namespace DataConfiguration;
 
 public class ApplicationDbContext : DbContext
 {
-    public ApplicationDbContext( )
+    public ApplicationDbContext()
     {
-        
+
     }
     public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
     {
 
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        base.OnConfiguring(optionsBuilder);
+       
     }
 }
--- web/backend/Web.Api/Program.cs
+++ web/backend/Web.Api/Program.cs
@@ -1,11 +1,16 @@
+using DataConfiguration;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
  builder.Services.AddOpenApi();
 builder.Services.AddControllers();
 
+builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("default")));
+
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+ 
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
